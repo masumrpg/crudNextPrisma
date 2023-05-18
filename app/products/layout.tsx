ceb1e7a71bf5d4143fad18f4f08c0957bb355f35
masumrpg <masumrpg@gmail.com>
2023-05-18 09:32:00 +0700

    ok

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -3,9 +3,9 @@ export const metadata = {
 };
 
 const ProductLayout = ({children}: {children: React.ReactNode}) => {
-  return (
-    <div className="py-10 px-10">{children}</div>
-  )
+    return (
+        <div className="py-10 px-10">{children}</div>
+    )
 };
 
 export default ProductLayout;
