2729c280ee6bd17d1014fb4c452e93d7383a46c0
masumrpg <masumrpg@gmail.com>
2023-05-20 06:49:19 +0700

    ws lah

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import Link from "next/link"
+
 const Home = () => {
+  const link = '/products';
+
   return (
-    <div>Halaman Utama</div>
+    <>
+    <head>
+        <title>Home Page</title>
+        <meta name="description" content="Deskripsi halaman Anda" />
+        {/* Tambahkan metadata lainnya di sini */}
+    </head>
+    <div className="flex justify-center pt-80">
+      <Link href={link} className="btn btn-primary">Products</Link>
+    </div>
+    </>
   )
 }
 
--- app/products/addProduct.tsx
+++ app/products/addProduct.tsx
@@ -3,6 +3,7 @@ import { SyntheticEvent, useState } from "react";
 import {Brand} from "@prisma/client"
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import Link from "next/link";
 
 
 const AddProduct = ({brands}: {brands: Brand[]}) => {
@@ -32,10 +33,13 @@ const AddProduct = ({brands}: {brands: Brand[]}) => {
         setIsOpen(!isOpen);
     }
 
+    const homePage = '/'
+
 
     return (
     <div>
-        <button className="btn" onClick={handleModal}>Tambah</button>
+        <button className="btn mr-3" onClick={handleModal}>Tambah</button>
+        <Link className="btn btn-primary bg-sky-500 hover:bg-sky-700" href={homePage}>Home Page</Link>
 
         <div className={isOpen ? 'modal modal-open' : 'modal'}>
             <div className="modal-box">
@@ -43,15 +47,15 @@ const AddProduct = ({brands}: {brands: Brand[]}) => {
                 <form onSubmit={handleSubmit}>
                     <div className="form-control w-full">
                         <label className="label font-bold">Nama Produk</label>
-                        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="input input-bordered" placeholder="Product Name" />
+                        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="input input-bordered" placeholder="Product Name" required />
                     </div>
                     <div className="form-control w-full">
                         <label className="label font-bold">Harga</label>
-                        <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} className="input input-bordered" placeholder="Price" />
+                        <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} className="input input-bordered" placeholder="Price" required />
                     </div>
                     <div className="form-control w-full">
                         <label className="label font-bold">Merek</label>
-                        <select value={brand} onChange={(e) => setBrand(e.target.value)} className="select select-bordered">
+                        <select value={brand} onChange={(e) => setBrand(e.target.value)} className="select select-bordered" required>
                             <option value="" disabled>Pilih Merek</option>
                             {brands.map((brand) => (
                                 <option value={brand.id} key={brand.id}>{brand.name}</option>
